e432b7c81dee060795f2b7af75df3029a7c75dc2
carlos_mesquita <carlossmessquita@gmail.com>
2021-11-30 15:15:16 -0300

    Melhorando Validações

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -10,11 +10,11 @@ class ClienteSerializer(serializers.ModelSerializer):
 
     def validate(self, data):
         if not cpf_valido(data['cpf']):
-            raise serializers.ValidationError({"cpf": "O CPF deve ter 11 dígitos"})
-        if nome_valido(data['nome']):
+            raise serializers.ValidationError({"cpf": "N° de CPF inválido! "})
+        if not nome_valido(data['nome']):
             raise serializers.ValidationError({'nome': 'Não inclua números no campo Nome'})
         if not rg_valido(data['rg']):
             raise serializers.ValidationError({'rg': 'O RG deve ter 9 dígitos'})
-        if celular_valido(data['celular']):
-            raise serializers.ValidationError({'celular': 'O celular dever conter no mínimo 11 dígitos'})
+        if not celular_valido(data['celular']):
+            raise serializers.ValidationError({'celular': 'Adote o padrão: XX XXXXX-XXXX'})
         return data
