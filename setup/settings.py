3d831d516dfbe9036d9b8e8bd16dee5ad487a139
carlos_mesquita <carlossmessquita@gmail.com>
2021-11-30 15:42:36 -0300

    Add filtros de busca

--- setup/settings.py
+++ setup/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'clientes',
+    'django_filters',
+
 ]
 
 MIDDLEWARE = [
@@ -120,3 +122,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Paginação:
+# 'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_frameworkDjangoFilterBackend']**
+REST_FRAMEWORK = {
+
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
